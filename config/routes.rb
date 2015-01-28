f1d8583fff53f3e354ab61e55d9e30bfca4c8e5a
Dillon Good <DCGood@Air-for-days.local>
2015-01-28 16:32:27 -0700

    Commit through Terms

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'welcome#index'
   get 'terms/' => 'terms#index'
-  get 'about/' => 'about#'
-  
+  get 'about/' => 'about#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
